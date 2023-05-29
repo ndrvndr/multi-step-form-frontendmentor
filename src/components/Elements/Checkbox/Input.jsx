06d3e87616ce8112cebf7ef9a0d7f7717dcd2f8f
ndrvndr <andreavindra37@gmail.com>
2023-05-29 15:54:12 +0700

    create checkbox component

--- src/components/Elements/Checkbox/Input.jsx
+++ src/components/Elements/Checkbox/Input.jsx
@@ -1,13 +1,18 @@
 import PropTypes from "prop-types";
+import IconCheckmark from "../../../assets/images/icon-checkmark.svg";
 
 const Input = ({ isClicked, handleClick }) => {
   return (
-    <input
-      type="checkbox"
-      checked={isClicked}
-      onChange={handleClick}
-      className="mr-5 w-5"
-    />
+    <div
+      onClick={handleClick}
+      className={`mr-5 flex h-5 w-5 items-center justify-center self-center rounded-sm border border-solid ${
+        isClicked
+          ? "border-primary-purplish-blue bg-primary-purplish-blue"
+          : "border-neutral-light-gray bg-neutral-white"
+      }`}
+    >
+      <img src={IconCheckmark} alt='Icon Checkmark' />
+    </div>
   );
 };
 
